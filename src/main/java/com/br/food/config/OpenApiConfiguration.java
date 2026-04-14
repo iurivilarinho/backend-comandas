81ec4370e14a804e49e5fac43788a121938e51c8
Iuri Vilarinho <iuri-vilarinho@hotmail.com>
2026-04-14 19:06:45 -0300

    feat: ajuste documentação swegger

--- src/main/java/com/br/food/config/OpenApiConfiguration.java
+++ src/main/java/com/br/food/config/OpenApiConfiguration.java
@@ -3,17 +3,25 @@ package com.br.food.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfiguration {
 
-    @Bean
-    public OpenAPI barTabsOpenApi() {
-        return new OpenAPI().info(new Info()
-                .title("Bar Tabs API")
-                .description("API for managing tabs, tables, stock, and sales in bars and restaurants.")
-                .version("1"));
-    }
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+
+				.components(new Components().addSecuritySchemes("bearer-key",
+						new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+				.info(new Info().title("API Sistema de Comandas")
+						.description("API Rest da aplicação de gestão de comandas").version("1")
+						.contact(new Contact().name("Time Backend").email("iuri-vilarinho@hotmail.com"))
+						.license(new License().name("Apache 2.0").url("http://lis.com/api/licenca")));
+	}
 }
