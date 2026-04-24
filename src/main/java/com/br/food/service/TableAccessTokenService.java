31c050a7606b6ec80e4375a3647e1b068bcc032a
Iuri Vilarinho <iuri-vilarinho@hotmail.com>
2026-04-24 17:24:39 -0300

    feat: remoção da expiração do token do qrcode da mesa

--- src/main/java/com/br/food/service/TableAccessTokenService.java
+++ src/main/java/com/br/food/service/TableAccessTokenService.java
@@ -1,15 +1,12 @@
 package com.br.food.service;
 
-import java.time.Instant;
-
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-
-import org.springframework.dao.DataIntegrityViolationException;
 
 @Service
 public class TableAccessTokenService {
@@ -21,23 +18,14 @@ public class TableAccessTokenService {
 	}
 
 	public String generate(String tableNumber) {
-		return JWT.create()
-				.withIssuer("food-table-access")
-				.withSubject("table-access")
-				.withClaim("tableNumber", tableNumber)
-				.withExpiresAt(Instant.now().plusSeconds(60L * 60L * 24L * 30L))
-				.sign(algorithm);
+		return JWT.create().withIssuer("food-table-access").withSubject("table-access")
+				.withClaim("tableNumber", tableNumber).sign(algorithm);
 	}
 
 	public String resolve(String token) {
 		try {
-			return JWT.require(algorithm)
-					.withIssuer("food-table-access")
-					.withSubject("table-access")
-					.build()
-					.verify(token)
-					.getClaim("tableNumber")
-					.asString();
+			return JWT.require(algorithm).withIssuer("food-table-access").withSubject("table-access").build()
+					.verify(token).getClaim("tableNumber").asString();
 		} catch (JWTVerificationException exception) {
 			throw new DataIntegrityViolationException("Invalid table access token.");
 		}
